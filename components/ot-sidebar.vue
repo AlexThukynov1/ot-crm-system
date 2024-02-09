bd6d8126a0d243213651f6d6e9f210a31a6c6ac3
Tsykunov Oleksandr <50213824+AlexThukynov1@users.noreply.github.com>
2024-02-09 17:49:07 +0200

    Add files via upload

---
+++ components/ot-sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+    <aside>
+        <nuxt-link to="/">
+            <NuxtImg/>
+        </nuxt-link>
+    </aside>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+    <section 
+        class="grid"
+        style="min-height:100vh"
+    >
+        <div></div>
+        <div>
+            <slot></slot>
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.grid {
+    display: grid;
+    grid-template-columns: 1fr 6fr;
+}
+</style>
