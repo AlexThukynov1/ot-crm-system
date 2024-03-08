3f41a96ebdaee3aaf780ce8737f76b436d80dd8a
AlexThukynov1 <alexthukynov@gmail.com>
2024-03-09 00:46:54 +0200

    add back-end settings

--- lib/app.constants.ts
+++ lib/app.constants.ts
@@ -1,4 +1,7 @@
 
 export const APP_WRITE_ID = '65ea3e2e766c9dc7c6db';
-export const DB_ID = '';
-export const COLLECTIONS_DEALS = ''
+export const DB_ID = 'ot-crm-system';
+export const COLLECTIONS_DEALS = 'deals';
+export const COLLECTION_CUSTOMERS = 'customers';
+export const COLLECTION_COMMENTS = 'comments';
+export const STORAGE_ID = 'ot-crm-system-storage';
